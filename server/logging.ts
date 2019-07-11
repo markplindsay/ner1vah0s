d63fa37d19e9b356aa2c2d410de80e1edaf570d3
Mark Lindsay <mark@nervous.org>
2019-07-11 00:45:48 +0000

    Logging tweaks

--- server/logging.ts
+++ server/logging.ts
@@ -4,6 +4,7 @@ import { Socket } from '../types'
 export const logSocketEvent = (action: string, socket: Socket): void => {
   console.log(JSON.stringify({
     date: new Date().toISOString(),
+    message: `Socket ${action} from ${socket.client.conn.remoteAddress}`,
     socketId: socket.id,
     socketEvent: action,
     status: 'info',
@@ -12,25 +13,25 @@ export const logSocketEvent = (action: string, socket: Socket): void => {
 }
 
 export const morganMiddleware = () => morgan((tokens, req, res) => {
-  const httpStatus = parseInt(tokens.status(req, res), 10)
-  let status = 'info'
-  if (httpStatus > 399) {
-    status = 'warn'
-  }
-  else if (httpStatus > 499) {
-    status = 'error'
+  const httpStatus = tokens.status(req, res)
+  const httpVersion = tokens['http-version'](req, res)
+  const method = tokens.method(req, res)
+  const url = tokens.url(req, res)
+  let remoteAddress = tokens['remote-addr'](req, res)
+  if (process.env.NODE_ENV === 'production') {
+    remoteAddress = tokens.req(req, res, 'X-Real-Ip')
   }
   return JSON.stringify({
     contentLength: tokens.res(req, res, 'content-length'),
     date: new Date().toISOString(),
-    httpStatus: httpStatus.toString(),
-    httpVersion: tokens['http-version'](req, res),
-    method: tokens.method(req, res),
+    httpStatus,
+    httpVersion,
+    message: `"${method} ${url} HTTP/${httpVersion}" ${httpStatus}`, 
+    method,
     referrer: tokens.referrer(req, res),
-    remoteAddress: tokens['remote-addr'](req, res),
+    remoteAddress,
     responseTime: parseFloat(tokens['response-time'](req, res)) * 1000,
-    status,
-    url: tokens.url(req, res),
+    url,
     userAgent: tokens['user-agent'](req, res),
   })
 })
